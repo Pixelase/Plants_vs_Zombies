b89b014b603bb400a3cd39d6c3c731959108ee26
Alexander Babaj <sustem34@gmail.com>
2015-04-15 18:30:21 +0300

    Enemies new 3D models
    * Добавлены модели для всех типов врагов;
    * Анимация состояний врагов;
    * Удалены старые ресурсы;

--- Assets/Scripts/EnemyDamage.cs
+++ Assets/Scripts/EnemyDamage.cs
@@ -1,46 +1,50 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class EnemyDamage : MonoBehaviour {
+public class EnemyDamage : MonoBehaviour
+{
 
-	private EnemyMove movscr;
 	public float damage;
 	public float Cooldown;
+
 	private float cd;
+	private EnemyMove movscr;
+	private Animator anim;
 
 	// Use this for initialization
-	void Start () {
-		movscr = gameObject.GetComponent<EnemyMove>();
+	void Start ()
+	{
+		movscr = gameObject.GetComponent<EnemyMove> ();
+		anim = gameObject.GetComponentInChildren<Animator> ();
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		if(cd > 0)
-		{
+	void Update ()
+	{
+		if (cd > 0) {
 			cd -= Time.deltaTime;
 		}
 		RaycastHit hit;
-		if(Physics.Raycast(transform.position, Vector3.left, out hit, 0.5F))
-		{
-			if(hit.transform.tag == "Tower")
-			{
-				if(cd <= 0)
-				{
-					Health hscr = hit.transform.gameObject.GetComponent<Health>();
+		if (Physics.Raycast (transform.position, Vector3.left, out hit, 0.5F)) {
+			if (hit.transform.tag == "Tower") {
+				if (cd <= 0) {
+					if (anim != null) {
+						anim.SetBool ("hasTower", true);
+					}
+
+					Health hscr = hit.transform.gameObject.GetComponent<Health> ();
 					hscr.health -= damage;
 					cd = Cooldown;
-				}
-				
-			}
-			else if(hit.transform.tag == "House")
-			{
-				GameObject.Find("GameLogic").GetComponent<GameOver>().lost = true;
+				}				
+			} else if (hit.transform.tag == "House") {
+				GameObject.Find ("GameLogic").GetComponent<GameOver> ().lost = true;
 			}
 			movscr.canMove = false;
-		}
-		else if(movscr.canMove == false)
-		{
+		} else if (movscr.canMove == false) {
 			movscr.canMove = true;
+			if(anim != null){
+				anim.SetBool("hasTower", false);
+			}
 		}
 	}
 }
--- Assets/Scripts/EnemyMove.cs
+++ Assets/Scripts/EnemyMove.cs
@@ -5,12 +5,28 @@ public class EnemyMove : MonoBehaviour {
 
 	public float MovementSpeed;
 	public bool canMove;
-	
+
+	private Animator anim;
+
+	void Start()
+	{
+		anim = gameObject.GetComponentInChildren<Animator>();
+	}
+
 	// Update is called once per frame
 	void Update () {
 		if(canMove)
 		{
+			if(anim != null)
+			{
+				anim.SetBool("canMove", true);
+			}
+
 			transform.Translate(Vector3.left * MovementSpeed * Time.deltaTime);	
+		}
+		else if(anim != null)
+		{		
+			anim.SetBool("canMove", false);
 		}
 	}
 }
