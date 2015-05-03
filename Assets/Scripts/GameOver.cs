987e8149e29e8124e97b30c5d6cbdd495f57c76d
Alexander Babaj <sustem34@gmail.com>
2015-05-03 16:33:01 +0300

    GameOver splash screen
    * Анимировал заставку GameOver;
    * Переписал GameOver скрипт;
    * Добавил кнопку Restart;

--- Assets/Scripts/GameOver.cs
+++ Assets/Scripts/GameOver.cs
@@ -5,54 +5,42 @@ public class GameOver : MonoBehaviour
 {
 	public bool lost;
 
-	private Money mscr;
-	private float initMoney;
 	private WaveManager wavscr;
-	private GameObject[] tiles;
+	private Animator anim;
 
 	void Start ()
 	{
-		mscr = gameObject.GetComponent<Money>();
-		wavscr = gameObject.GetComponent<WaveManager>();
-		initMoney = mscr.money;
-		tiles = GameObject.FindGameObjectsWithTag("Tile");
+		wavscr = gameObject.GetComponent<WaveManager> ();
+		anim = GameObject.Find ("Canvas").GetComponent<Animator> ();
 	}
 	
 	// Update is called once per frame
 	void Update ()
 	{
 		if (lost) {
-			lost = false;
-			wavscr.wavesCount = 0;
-			GameObject[] towers = GameObject.FindGameObjectsWithTag("Tower");
-			GameObject[] enemies = GameObject.FindGameObjectsWithTag("Enemy");
+			GameObject[] towers = GameObject.FindGameObjectsWithTag ("Tower");
+			GameObject[] enemies = GameObject.FindGameObjectsWithTag ("Enemy");
+			GameObject[] projs = GameObject.FindGameObjectsWithTag ("Projectile");
 
-			wavscr.CancelInvoke();
-			wavscr.Cooldown = 4;
-			wavscr.InvokeRepeating("Spawn", wavscr.iniPause, wavscr.Cooldown);
-
-			foreach(GameObject tile in tiles)
-			{
-				tile.GetComponent<TileTaken>().isTaken = false;
-			}
+			anim.SetTrigger("GameOver");
+			wavscr.CancelInvoke ();
 
 			for (int i = 0; i < towers.Length; i++) {
-				Destroy (towers[i]);
+				Destroy (towers [i]);
 			}
 
 			for (int j = 0; j < enemies.Length; j++) {
-				Destroy (enemies[j]);
+				Destroy (enemies [j]);
 			}
-
-
-
-			GameObject[] projs = GameObject.FindGameObjectsWithTag("Projectile");
 
 			for (int k = 0; k < projs.Length; k++) {
-				Destroy (projs[k]);
+				Destroy (projs [k]);
 			}
-
-			mscr.money = initMoney;
 		}
+	}
+
+	public void Restart()
+	{
+		Application.LoadLevel(0);
 	}
 }
